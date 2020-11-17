0f410947e649eb1c9deaf8d5d9722cd5ed2ba83f
ojw209 <ojw209>
2020-11-17 14:21:39 +0000

    Pandas Data Frame Created
    Pandas DF created - now working on trying to pre-proscess text to remove commonn words (I.E The, NEWS, Lottery)

--- Main.py
+++ Main.py
@@ -1,17 +1,10 @@
 import requests
-import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
+from collections import Counter
 
-base_url = "https://www.thepaperboy.com"
-start_postfix_url = "/uk/2020/01/01/front-pages-archive.cfm"
-start_url = base_url + start_postfix_url
-url = start_url
 
-#How many months to scan for
-Start_Date = '2020-01-01'
-Months2Scan = 1
-
+#Function scrapes 'ThePaperBoy' for a list of urls for newspaper front pages.
 def Address_Scraper(start_url):
     
     url = start_url
@@ -61,54 +54,82 @@ def Address_Scraper(start_url):
     
     return Daily_Overview_List, Front_Page_List
 
+#Functions scrapes a list of  front page urls for there article infomation.
+def Page_Scrapper(Front_Page_List):
+    
+    #List of urls that triggered errors and thus no data was retrieved from.
+    Url_Error_List = []
+    
+    #Temporary lists storing scrapped data, ready for putting into pandas
+    Paper_Type_List = []
+    Paper_Date_List = []
+    Paper_Article_List = []
+        
+    for page_url in Front_Page_List:
+        
+        url = base_url + page_url
+        
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        
+        Temp_Article = []
+        
+        #Extract data from news page - including paper type and date.
+        try:
+            
+            Meta_Page_Text = soup.find('h1').text
+        
+            Temp_Paper_Type = Meta_Page_Text.partition("Headlines")[0][:-1]
+            Temp_Paper_Date = Meta_Page_Text.partition("Headlines")[2][1:]
+            
+            Paper_Type_List.append(Temp_Paper_Type)
+            Paper_Date_List.append(Temp_Paper_Date)
+            
+            #Extract rss feeds from newspaper page - equates to around 5 or 6 news articles per a page.
+            for newsrss in soup.findAll('div', id="rssfeed"):
+                
+                Temp_Article.append(newsrss.text)
+                #.split()
+            #Creates a list of lists of news articles for that page.
+            Paper_Article_List.append(Temp_Article)
+                
+        except:       
+            
+            print("Error at:", url)
+            Url_Error_List.append(page_url)
+        
+    
+    #Create a pandas dataframe for the newspaper name and date.
+    Paper_Data = {'Date': Paper_Date_List, 'Paper Name':Paper_Type_List}
+    Paper_Data_Store = pd.DataFrame(Paper_Data)
+    
+    #Create a pandas dataframe just for the newspaper articles
+    Article_Data_Store = pd.DataFrame(Paper_Article_List, columns = [('Art' + str(i)) for i in range(5)]) 
+    
+    #Merge these 2 dataframes.
+    Main_Store =  pd.concat([Paper_Data_Store, Article_Data_Store], axis=1, join='inner')
+    
+    
+    
+    return Main_Store, Url_Error_List
+
+
+#Define Starting URL to start scrapping from.
+base_url = "https://www.thepaperboy.com"
+start_postfix_url = "/uk/2020/01/01/front-pages-archive.cfm"
+start_url = base_url + start_postfix_url
+
+#How many months to scan for
+Start_Date = '2020-01-01'
+Months2Scan = 1
 
 Daily_Overview_List, Front_Page_List = Address_Scraper(start_url)
 
-#List of urls that triggered errors and thus no data was retrieved from.
-Url_Error_List = []
+Main_Store, Url_Error_List = Page_Scrapper(Front_Page_List)
 
-#Temporary lists storing scrapped data, ready for putting into pandas
-Paper_Type_List = []
-Paper_Date_List = []
-Paper_Article_List = []
+#NEED TO GENERALISE LINE - [Article Count fixed to 5 ]
+Main_Store['FullArt'] = Main_Store['Art0'] + Main_Store['Art1'] + Main_Store['Art2'] + Main_Store['Art3'] + Main_Store['Art4']
+Main_Store['FullArt'].count('BREXIT')
 
-
-#    
-for page_url in Front_Page_List:
-    
-    url = base_url + page_url
-    
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    
-    #Extract data from news page - including paper type and date.
-    try:
-        
-        Meta_Page_Text = soup.find('h1').text
-    
-        Temp_Paper_Type = Meta_Page_Text.partition("Headlines")[0][:-1]
-        Temp_Paper_Date = Meta_Page_Text.partition("Headlines")[2][1:]
-        
-        Paper_Type_List.append(Temp_Paper_Type)
-        Paper_Date_List.append(Temp_Paper_Date)
-        
-    except:       
-        
-        print("Error at:", url)
-        Url_Error_List.append(page_url)
-    
-    Temp_Article = []
-    
-    #Extract rss feeds from newspaper page - equates to around 5 or 6 news articles per a page.
-    try:   
-        
-        for newsrss in soup.findAll('div', id="rssfeed"):
-            
-            Temp_Article.append(newsrss.text)
-        
-        #Creates a list of lists of news articles for that page.
-        Paper_Article_List.append(Temp_Article)
-    
-    except ConnectionError:
-        print("Error Connection Error")
+test = Counter(Main_Store['FullArt'][1].split()).most_common()
 
